bf813a49f4ab1539a9ecb2f70e60fd6dfa3a04e9
luisda190519 <licerol@uninorte.edu.co>
2022-09-17 11:13:51 -0500

    Adding stuffs

--- public/javascript/deaths/deaths.js
+++ public/javascript/deaths/deaths.js
@@ -1,4 +1,10 @@
-let deathData = { totalDeathsDatasets: [], labels: [], newDeathDataSets: [] };
+let deathData = {
+  totalDeathsDatasetsByCountry: [],
+  labels: [],
+  newDeathDataSetsByCountry: [],
+  newDeathDataSetsGlobal: [],
+  totalDeathsDatasetGlobal: [],
+};
 
 const randomColor = function () {
   r = Math.floor(Math.random() * 256);
@@ -25,6 +31,8 @@ const createTotalDeathsData = function (data) {
   let inside = new Object();
   let lastCountry = data[0].country;
   let array = [];
+  let total = [];
+  let cont = 0;
 
   data.forEach((deathCase) => {
     if (!(lastCountry == deathCase.country)) {
@@ -33,20 +41,37 @@ const createTotalDeathsData = function (data) {
       inside.radius = 0;
       inside.label = deathCase.country;
       inside.data = array;
-      deathData.totalDeathsDatasets.push(inside);
+      deathData.totalDeathsDatasetsByCountry.push(inside);
 
       array = [];
+      cont = 0;
       inside = new Object();
     }
+
+    if (total[cont] === undefined) {
+      total[cont] = 0;
+    }
+
     array.push(deathCase.totalDeaths);
+    total[cont] += deathCase.totalDeaths;
     lastCountry = deathCase.country;
+    cont++;
   });
+
+  inside.borderColor = randomColor();
+  inside.borderWidth = 1;
+  inside.radius = 0;
+  inside.label = "World";
+  inside.data = total;
+  deathData.totalDeathsDatasetGlobal.push(inside);
 };
 
 const createNewDeathsData = function (data) {
   let inside = new Object();
   let lastCountry = data[0].country;
   let array = [];
+  let total = [];
+  let cont = 0;
 
   data.forEach((deathCase) => {
     if (!(lastCountry == deathCase.country)) {
@@ -55,49 +80,65 @@ const createNewDeathsData = function (data) {
       inside.radius = 0;
       inside.label = deathCase.country;
       inside.data = array;
-      deathData.newDeathDataSets.push(inside);
+      deathData.newDeathDataSetsByCountry.push(inside);
 
       array = [];
       inside = new Object();
-      stop = false;
+      cont = 0;
     }
 
     array.push(deathCase.newDeaths);
     lastCountry = deathCase.country;
+
+    if (total[cont] === undefined) {
+      total[cont] = 0;
+    }
+    total[cont] += deathCase.newDeaths;
+    cont++;
   });
+
+  inside.borderColor = randomColor();
+  inside.borderWidth = 1;
+  inside.radius = 0;
+  inside.label = "World";
+  inside.data = total;
+  deathData.newDeathDataSetsGlobal.push(inside);
 };
 
-const addTotalChart = async function () {
-  totalDeathConfig = { ...config };
-  totalDeathConfig.data.datasets = deathData.totalDeathsDatasets;
-  totalDeathConfig.data.labels = deathData.labels;
-  totalDeathConfig.options.plugins.title = "COVID-19 total world deaths";
+const addDeathsChart = async function (title, data, selectedChart) {
+  config.data.datasets = data;
+  config.options.plugins.title = title;
+  config.data.labels = deathData.labels;
 
-  const myChart = await new Chart(
-    document.getElementById("myChart"),
-    totalDeathConfig
+  const chart = await new Chart(document.getElementById(selectedChart), config);
+};
+
+const createData = async function () {
+  const result = await fetch("http://localhost:3000/deaths/deathStatics");
+  const data = await result.json();
+  addLabels(data);
+  createTotalDeathsData(data);
+  createNewDeathsData(data);
+  addDeathsChart(
+    "COVID-19 total world deaths by country",
+    deathData.totalDeathsDatasetsByCountry,
+    "myChart1"
+  );
+  addDeathsChart(
+    "COVID-19 total new world deaths by country",
+    deathData.newDeathDataSetsByCountry,
+    "myChart3"
+  );
+  addDeathsChart(
+    "COVID-19 global total world deaths",
+    deathData.totalDeathsDatasetGlobal,
+    "myChart2"
+  );
+  addDeathsChart(
+    "COVID-19 total global new world deaths",
+    deathData.newDeathDataSetsGlobal,
+    "myChart4"
   );
 };
 
-const addNewChart = async function () {
-  newDeathConfig = { ...config };
-  newDeathConfig.data.datasets = deathData.newDeathDataSets;
-  newDeathConfig.options.plugins.title = "COVID-19 total new world deaths";
-  newDeathConfig.data.labels = deathData.labels;
-
-  const myChart2 = await new Chart(
-    document.getElementById("myChart2"),
-    newDeathConfig
-  );
-};
-
-fetch("http://localhost:3000/deaths/deathStatics")
-  .then((result) => result.json())
-  .then((data) => {
-    addLabels(data);
-    createTotalDeathsData(data);
-    createNewDeathsData(data);
-    addTotalChart();
-    addNewChart();
-  })
-  .catch((err) => console.log(err));
+createData();
--- routes/covidDeathsRoutes.js
+++ routes/covidDeathsRoutes.js
@@ -3,8 +3,18 @@ const mongoose = require("mongoose");
 const covidDeaths = require("../models/covidDeaths");
 const router = express.Router();
 
-const searchDeathCases = async () => {
+const filter = { global: false, byCountry: false, error: false };
+let location = "";
+
+const searchDeathCases = async function () {
   const deaths = await covidDeaths.find().sort({ country: 1, date: 1 });
+  return deaths;
+};
+
+const searchDeathCasesByCountry = async function (country) {
+  const deaths = await covidDeaths
+    .find({ country: country })
+    .sort({ country: 1, date: 1 });
   return deaths;
 };
 
@@ -16,7 +26,17 @@ const GetTotalDeaths = function (deaths) {
   return total;
 };
 
-function commafy(num) {
+const GetTotalDeathsByCountry = function (deaths, country) {
+  let total = 0;
+  deaths.forEach((deathCase) => {
+    if (deathCase.country === country) {
+      total += deathCase.totalDeaths;
+    }
+  });
+  return total;
+};
+
+const commafy = function commafy(num) {
   let str = num.toString().split(".");
   if (str[0].length >= 5) {
     str[0] = str[0].replace(/(\d)(?=(\d{3})+$)/g, "$1,");
@@ -25,23 +45,42 @@ function commafy(num) {
     str[1] = str[1].replace(/(\d{3})/g, "$1 ");
   }
   return str.join(".");
-}
+};
 
-router
-  .route("/")
-  .get(async (req, res) => {
-    const deaths = await searchDeathCases();
-    const totalDeaths = commafy(GetTotalDeaths(deaths));
-    res.render("./templates/deaths", { deaths, totalDeaths });
-  })
+router.route("/filter/:filter").get(async (req, res) => {
+  const deaths = await searchDeathCases();
+  const totalDeaths = commafy(GetTotalDeaths(deaths));
+  if (req.params.filter === "global") {
+    filter.global = true;
+    filter.byCountry = false;
+  } else if (req.params.filter === "byCountry") {
+    filter.byCountry = true;
+    filter.global = false;
+  }
+  res.render("./templates/deaths", { deaths, totalDeaths, filter });
+});
 
-  .post(async (req, res) => {
-    const deaths = await searchDeathCases();
-    res.json(deaths);
-  });
+router.route("/search").get(async (req, res) => {
+  let { country } = req.query;
+  const deaths = await searchDeathCasesByCountry(country);
+  location = country;
+  const totalDeaths = commafy(GetTotalDeathsByCountry(deaths, country));
+
+  if (totalDeaths == 0) {
+    filter.error = true;
+    res.redirect("/deaths/filter/global");
+  } else {
+      res.render("./templates/deathsCountry", { deaths, totalDeaths, country });
+  }
+});
 
 router.get("/deathStatics", async (req, res) => {
   const deaths = await searchDeathCases();
+  res.json(deaths);
+});
+
+router.get("/deathStaticsByCountry", async (req, res) => {
+  const deaths = await searchDeathCasesByCountry(location);
   res.json(deaths);
 });
 
